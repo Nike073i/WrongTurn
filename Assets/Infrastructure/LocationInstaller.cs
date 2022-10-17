f853d7bbf70afd9f59373df1d406998c5ba9f9ef
Nike073i <nike073i@mail.ru>
2022-10-17 18:50:19 +0400

    Вынесение точки финиша в инсталлер локации

--- Assets/Infrastructure/LocationInstaller.cs
+++ Assets/Infrastructure/LocationInstaller.cs
@@ -5,14 +5,19 @@ public class LocationInstaller : MonoInstaller
 {
     [SerializeField]
     private GameObject _playerPrefab;
-
     [SerializeField]
     private Transform _playerStartPoint;
+
+    [SerializeField]
+    private GameObject _finishPrefab;
+    [SerializeField]
+    private Transform _finishPoint;
+
 
     public override void InstallBindings()
     {
         BindPlayer();
-
+        BindFinishPoint();
     }
 
     private void BindPlayer()
@@ -20,6 +25,14 @@ public class LocationInstaller : MonoInstaller
         var playerController = Container.InstantiatePrefabForComponent<PlayerController>(_playerPrefab, _playerStartPoint);
         Container.Bind(typeof(PlayerController))
             .FromInstance(playerController)
+            .AsSingle();
+    }
+
+    private void BindFinishPoint()
+    {
+        var finishZone = Container.InstantiatePrefabForComponent<FinishZone>(_finishPrefab, _finishPoint);
+        Container.Bind(typeof(FinishZone))
+            .FromInstance(finishZone)
             .AsSingle();
     }
 }
